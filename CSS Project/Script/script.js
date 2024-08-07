79892b2eaffcd672342cd2c00209fbda2387d980
vedantgandhi27 <vedantgandhi2772@gail.com>
2024-08-07 17:29:22 -0400

    Added some media queries to 2 product page

--- CSS Project/Script/script.js
+++ CSS Project/Script/script.js
@@ -5,3 +5,19 @@ function toggleMenu(){
     menu.classList.toggle("show_class");
     document.body.classList.toggle("no-scroll");
 }
+
+// Code for product details page for showing different view of the product images
+let var1 = document.querySelectorAll(".contact_topic")[0];
+let var2 = document.querySelectorAll(".contact_topic")[1];
+let var3 = document.querySelectorAll(".contact_topic")[2];
+
+
+//Grabs the Ipad images and enlarges it
+let picture = document.querySelectorAll(".preview_angles>img");
+picture.forEach(pic => {
+    pic.addEventListener("click",function(){
+        let mnimg = document.querySelector(".ipad-assests>img");
+        // shows the image
+        mnimg.setAttribute("src",pic.getAttribute("src"));
+    });
+}); 
